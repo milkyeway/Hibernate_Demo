22d9549cb7d7d8ce0adabc67ed0f6c117d07de3b
milkyeway <namilkywaw@gmail.com>
2022-03-10 15:25:47 +0800

    DemoOneToOneInstructorActionEx5
    DemoOneToOneInstructorActionEx5

--- workspace/Hibernate/src/com/hibernatedemo/action/DemoOneToOneInstructorActionEx3.java
+++ workspace/Hibernate/src/com/hibernatedemo/action/DemoOneToOneInstructorActionEx3.java
@@ -3,6 +3,7 @@ package com.hibernatedemo.action;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import com.hibernatedemo.model.Instructor;
 import com.hibernatedemo.model.InstructorDetail;
 import com.hibernatedemo.util.HibernateUtils;
 
@@ -15,10 +16,13 @@ public class DemoOneToOneInstructorActionEx3 {
 		try {
 			session.beginTransaction();
 
-			InstructorDetail detail101 = session.get(InstructorDetail.class, 105);
+			Instructor ins2 = session.get(Instructor.class, 2);
 
-			if (detail101 != null) {
-				System.out.println("NAME: " + detail101.getInstructor().getName());
+			InstructorDetail detail2 = ins2.getInstructorDetail();
+
+			if (detail2 != null) {
+				System.out.println("¥¿¦b§R°£: " + detail2.getInstructor().getName());
+				System.out.println(detail2);
 			}
 
 			session.getTransaction().commit();
