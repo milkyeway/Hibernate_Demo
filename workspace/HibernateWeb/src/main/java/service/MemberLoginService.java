24b92e396dcbd287d91d33e0b05b3abac902baf3
milkyeway <namilkywaw@gmail.com>
2022-03-11 16:54:24 +0800

    環境配置

--- workspace/HibernateWeb/src/main/java/service/MemberLoginService.java
+++ workspace/HibernateWeb/src/main/java/service/MemberLoginService.java
@@ -1,5 +1,23 @@
 package service;
 
-public class MemberLoginService {
+import model.Member;
+import model.MemberDao;
 
+public class MemberLoginService {
+	private MemberDao dao;
+
+	public MemberLoginService() {
+		this.dao = new MemberDao();
+	}
+
+	public Member checkLogin(String loginName, String loginPwd) {
+		Member memberBean = dao.findByNameAndPwd(loginName, loginPwd);
+
+		if (memberBean != null) {
+			return memberBean;
+		}
+
+		return null;
+
+	}
 }
