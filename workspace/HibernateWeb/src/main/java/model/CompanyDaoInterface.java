aff6731cc1a1635ba6e53234925174f74a1cf625
milkyeway <namilkywaw@gmail.com>
2022-03-11 15:13:24 +0800

    新增檔案

---
+++ workspace/HibernateWeb/src/main/java/model/CompanyDaoInterface.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.List;
+
+public interface CompanyDaoInterface {
+	CompanyBean insert(CompanyBean comBean);
+	CompanyBean select(int comId);
+	List<CompanyBean> selectAll();
+	CompanyBean updateOne(int comId, String comName);
+    boolean deleteOne(int id);
+}
---
+++ workspace/HibernateWeb/src/main/java/service/CompanyService.java
@@ -0,0 +1,46 @@
+package service;
+
+import java.util.List;
+import org.hibernate.Session;
+
+import model.CompanyBean;
+import model.CompanyDao;
+
+public class CompanyService implements CompanyServiceInterface {
+
+	private CompanyDao comDao;
+
+	public CompanyService(Session session) {
+		this.comDao = new CompanyDao(session);
+	}
+
+	@Override
+	public CompanyBean insert(CompanyBean comBean) {
+		CompanyBean oneCom = comDao.insert(comBean);
+		return oneCom;
+	}
+
+	@Override
+	public CompanyBean select(int comId) {
+		CompanyBean theCom = comDao.select(comId);
+		return theCom;
+	}
+
+	@Override
+	public List<CompanyBean> selectAll() {
+		return comDao.selectAll();
+	}
+
+	@Override
+	public CompanyBean updateOne(int comId, String comName) {
+		CompanyBean oneCom = comDao.updateOne(comId, comName);
+		return oneCom;
+	}
+
+	@Override
+	public Boolean delete(int comId) {
+		boolean boo = comDao.deleteOne(comId);
+		return boo;
+	}
+
+}
